be9daa9b20ef684e436c1f83a10eca1909f76b02
stocksignal <noobtrader81@gmail.com>
2021-06-06 12:57:30 +0530

    otp error solved

--- firebasePhone.js
+++ firebasePhone.js
@@ -1,22 +1,18 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyA6hJIywPAnSseehaBbng0abCxWFlLV_fw",
-    authDomain: "stock-signal-app.firebaseapp.com",
-    projectId: "stock-signal-app",
-    storageBucket: "stock-signal-app.appspot.com",
-    messagingSenderId: "638964824703",
-    appId: "1:638964824703:web:02d9bfb0e18099be841d4a",
-    measurementId: "G-WRNVEXM6ZP"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
-
-firebase.auth().languageCode = 'it';
+firebase.auth().languageCode = 'en';
 
 function onSignInSubmit(e){
     e.preventDefault();
-    console.log("onsubmit")
-    const phoneNumber = "+91"+document.getElementById('phone').value;
+    document.getElementById('login-submit').value = "Please wait..."
+    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('login-submit', {
+        'size': 'invisible',
+        'callback': (response) => {
+            console.log(response);
+        }
+    });
+
+    console.log("Number submitted");
+
+    const phoneNumber = "+91" + document.getElementById('phone').value;
     console.log(phoneNumber);
     const appVerifier = window.recaptchaVerifier;
     firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
@@ -41,39 +37,41 @@ function onSignInSubmit(e){
 
 function onotpsubmit(e){
     e.preventDefault();
+    document.getElementById('otp-submit').value = 'Please wait...';
     const code = document.getElementById('otp').value;
-        confirmationResult.confirm(code).then((result) => {
-        window.location = '/';
-        const user = result.user;
-        localStorage.setItem('ssuser', user['refreshToken']);
-        localStorage.setItem('phone', user['phoneNumber']);
-        localStorage.setItem('uid', user['uid']);
-        checklogged();
-        }).catch((error) => {
-            alert("Wrong OTP. Try again.")
-        });
+    confirmationResult.confirm(code).then((result) => {
+    document.getElementById('otp-submit').value = 'Logged in';
+    window.location = '/';
+    const user = result.user;
+    localStorage.setItem('ssuser', user['refreshToken']);
+    localStorage.setItem('phone', user['phoneNumber']);
+    localStorage.setItem('uid', user['uid']);
+    checklogged();
+    }).catch((error) => {
+        alert("Wrong OTP. Try again.")
+    });
 }
 
-function loginfire(e) {
-    e.preventDefault();
-    console.log("Clicked login button")
-  
-    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('login-submit', {
-        'size': 'invisible',
-        'callback': (response) => {
-            console.log(response);
-        }
-    });
-    onSignInSubmit(e);
-    
-}
+
 function checklogged(){
     loggeduser = localStorage.getItem('ssuser');
     if(loggeduser){
         document.getElementById('login-btn').style.display = 'none';
+        document.getElementById('oitable').style.display = 'block';
+        document.getElementById('oiChart').style.display = 'block';
+        document.getElementById('pcrChart').style.display = 'block';
+        document.querySelector('.logindialog1').style.display = 'none';
+        document.querySelector('.logindialog2').style.display = 'none';
+        document.querySelector('.logindialog3').style.display = 'none';
     }
     else{
         document.getElementById('login-btn').style.display = 'block';
+        document.getElementById('oitable').style.display = 'none';
+        document.getElementById('oiChart').style.display = 'none';
+        document.getElementById('pcrChart').style.display = 'none';
+        document.querySelector('.logindialog1').style.display = 'block';
+        document.querySelector('.logindialog2').style.display = 'block';
+        document.querySelector('.logindialog3').style.display = 'block';
     }
 }
 checklogged();
